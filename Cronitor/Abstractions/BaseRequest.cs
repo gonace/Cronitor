4aa731c340e7b20715bb7d969caeac4bba2143d9
Erik Hennerfors <erik.hennerfors@laget.se>
2023-12-21 14:34:56 +0100

    Update(s)

---
+++ Cronitor/Abstractions/BaseRequest.cs
@@ -0,0 +1,32 @@
+﻿using Cronitor.Constants;
+using Cronitor.Extensions;
+using System;
+using System.Net.Http;
+
+namespace Cronitor.Abstractions
+{
+    public abstract class BaseRequest
+    {
+        public HttpContent Content { get; set; }
+        public virtual HttpMethod Method { get; set; } = HttpMethod.Get;
+        public abstract string Endpoint { get; set; }
+
+
+        public BaseRequest SetContent(HttpContent content)
+        {
+            Content = content;
+
+            return this;
+        }
+
+        public virtual Uri ToUri()
+        {
+            return new Uri(Urls.ApiUrl.ToString()).Combine(Endpoint);
+        }
+
+        public string ToUrl()
+        {
+            return ToUri().ToString();
+        }
+    }
+}
