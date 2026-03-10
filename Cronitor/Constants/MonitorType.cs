5a60a573a47bd901404186a16187be36fdc608c9
Erik Hennerfors <erik.hennerfors@obscured.se>
2026-03-10 19:28:21 +0100

    Added MonitorType

---
+++ Cronitor/Constants/MonitorType.cs
@@ -0,0 +1,24 @@
+﻿namespace Cronitor.Constants
+{
+    public class MonitorType
+    {
+        public static readonly MonitorType Job = new MonitorType("job");
+        public static readonly MonitorType Check = new MonitorType("check");
+        public static readonly MonitorType Heartbeat = new MonitorType("heartbeat");
+        public static readonly MonitorType Site = new MonitorType("site");
+
+
+        public string Key { get; set; }
+
+
+        public MonitorType(string key)
+        {
+            Key = key;
+        }
+
+        public override string ToString()
+        {
+            return Key;
+        }
+    }
+}
